7e8e12a43d302d18748c0a1da21cf80b802d35a2
Izu-TABI <masakisama14@gmail.com>
2022-09-17 22:00:39 +0900

    [Update]home-main-title CSS

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Home() {
   return (
     <div className='home-main'>
+        <img src="" alt="" />
         <h1 className='home-main-title'>Izu-TABI</h1>
+        <ul>
+            <li>github</li>
+            <li>twitter</li>
+            <li>instagram</li>
+            <li>blog</li>
+        </ul>
     </div>
   )
 }
