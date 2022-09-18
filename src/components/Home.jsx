d168c6a2ea24d7402cdca14d35b54d99ec551646
Izu-TABI <masakisama14@gmail.com>
2022-09-18 11:47:01 +0900

    [Add]JSX and CSS for SNS icon Images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import GithubIcon from '../sns_icons_image/GitHub.png';
+import instaIcon from '../sns_icons_image/instagram.png';
+import twitterIcon from '../sns_icons_image/Twitter.png';
+import blogIcon from '../sns_icons_image/blog_icon.png';
 
 function Home() {
   return (
     <div className='home-main'>
-        <img src="" alt="" />
         <h1 className='home-main-title'>Izu-TABI</h1>
         <ul>
-            <li>github</li>
-            <li>twitter</li>
-            <li>instagram</li>
-            <li>blog</li>
+            <li><a href="https://github.com/Izu-TABI"><img src={GithubIcon} alt="" /></a></li>
+            <li><a href="https://twitter.com/chicken_dehanai"><img src={twitterIcon} alt="" /></a></li>
+            <li><a href="https://www.instagram.com/i.masaki14/"><img src={instaIcon} alt="" /></a></li>
+            <li><a href="https://izu-tabi.hatenablog.com/"><img src={blogIcon} alt="" /></a></li>
         </ul>
     </div>
   )
