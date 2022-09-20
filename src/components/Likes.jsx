12983d7f2636d1af6f8a6758c62b319375e7e2a8
Izu-TABI <masakisama14@gmail.com>
2022-09-20 23:27:26 +0900

    [Add]anchor link

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -14,13 +14,13 @@ function Likes() {
             <div className="contents">
 
                 <ul className="likes-list">
-                    <li>Apple</li>
+                    <li><a href="https://www.apple.com/">Apple</a></li>
                     <li>Computer</li>
-                    <li>JavaScript</li>
+                    <li><a href="https://developer.mozilla.org/ja/docs/Learn/JavaScript/First_steps/What_is_JavaScript">JavaScript</a></li>
                     <li>Web</li>
-                    <li>RealForce</li>
+                    <li><a href="https://www.realforce.co.jp/">RealForce</a></li>
                     <li>Anime</li>
-                    <li>BUMP OF CHICKEN</li>
+                    <li><a href="https://www.bumpofchicken.com/">BUMP OF CHICKEN</a></li>
                 </ul>
             </div>
         </div>
