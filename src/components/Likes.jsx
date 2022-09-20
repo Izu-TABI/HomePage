4ea758111b9d2d638053760e9527238522ead10b
Izu-TABI <masakisama14@gmail.com>
2022-09-20 21:54:06 +0900

    [Add]jsx for likes

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 function Likes() {
   return (
     <div className="likes-main">
-        
+        <div className="contents">
+            <h2 className="likes-title">
+                Likes
+            </h2>
+            <ul>
+                <li>Apple</li>
+                <li>Computer</li>
+                <li>JavaScript</li>
+                <li>Web</li>
+                <li>RealForce</li>
+                <li>Anime</li>
+                <li>BUMP OF CHICKEN</li>
+            </ul>
+        </div>
     </div>
   )
 }
