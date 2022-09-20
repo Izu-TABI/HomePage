d3e6905a0a0b5137dd1465b22539d835c5e8e876
Izu-TABI <masakisama14@gmail.com>
2022-09-20 22:17:28 +0900

    [Add]nav for Likes

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
+import Hamburger from './Hamburger'
+
 
 function Likes() {
   return (
-    <div className="main">
-        <h2 className="title">
-                Likes
-        </h2>
+    <>
+        <div className="main">
+            <Hamburger />
+            <h2 className="title">
+                    Likes
+            </h2>
 
-        <div className="contents">
+            <div className="contents">
 
-            <ul className="likes-list">
-                <li>Apple</li>
-                <li>Computer</li>
-                <li>JavaScript</li>
-                <li>Web</li>
-                <li>RealForce</li>
-                <li>Anime</li>
-                <li>BUMP OF CHICKEN</li>
-            </ul>
+                <ul className="likes-list">
+                    <li>Apple</li>
+                    <li>Computer</li>
+                    <li>JavaScript</li>
+                    <li>Web</li>
+                    <li>RealForce</li>
+                    <li>Anime</li>
+                    <li>BUMP OF CHICKEN</li>
+                </ul>
+            </div>
         </div>
-    </div>
+    </>
+
   )
 }
 
