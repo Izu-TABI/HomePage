1996aefaa6f4a36ddefac1e8733d77166832c87c
Izu-TABI <masakisama14@gmail.com>
2022-09-21 23:37:23 +0900

    [Add]jsx for Activities

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Hamburger from './Hamburger'
 
 function Activities() {
   return (
-    <div>Activities</div>
+    <>
+        <div className="main">
+            <Hamburger />
+            <h2 className="title">
+                    Activities
+            </h2>
+
+            <div className="contents">
+                
+            </div>
+        </div>
+    </>
   )
 }
 
