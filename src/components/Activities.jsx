b07a06a27982d072ec35b610ee44fc2cdeeecfb8
Izu-TABI <masakisama14@gmail.com>
2022-09-22 23:40:10 +0900

    [Add]jsx for activities

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -11,6 +11,16 @@ function Activities() {
             </h2>
 
             <div className="contents">
+                <div className="block-txt">
+                    <div className="block-date">
+                        <p>
+                            <span className="date">2022.3</span>
+                        </p>
+                    </div>
+                    <p className="tit">
+                        中国地区高専コンピュータフェスティバル
+                    </p>
+                </div>
                 
             </div>
         </div>
