f4cb4d430638f6b6ef4b5df81b3ab8216702806f
Izu-TABI <masakisama14@gmail.com>
2022-09-23 16:00:09 +0900

    [Add]CSS for works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,24 +4,27 @@ import Hamburger from './Hamburger'
 function Works() {
   return (
     <div>
-        <div className="main">
+        <div className="works-main">
             <Hamburger />
-            <h2 className="title">
+            <h2 className="works-title">
                     Works
             </h2>
 
-            <div className="contents">
+            <div className="works-contents">
                 <div className="work-block">
                     <a href="https://github.com/Izu-TABI/discord_bot_Log">Log</a>
                     <p>Node.js<br/>
                     Discordのbotでボイスチャンネルの入退出を記録するのが主な機能。<br/>
                     他にもボイスチャンネルに誰も参加してない場合メンションする機能などがある。
                     </p>
+                </div>
+                <div className="work-block">
                     <a href="https://github.com/Izu-TABI/discord_bot_weather">weather</a>
                     <p>Node.js<br/>
                         Discordのbotで天気予報をAPIを用いて送るbot
                     </p>
-                
+                </div>
+                <div className="work-block">
                     <a href="https://github.com/Izu-TABI/classic_games">CLASSIC GAMES</a>
                     <p>JavaScript HTML CSS jQuery 98.css<br/>
                         CSSのライブラリである98.cssを用いてwindows98の雰囲気を再現。有名ゲームを3つ収録。
