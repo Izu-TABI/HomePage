b5265ecfebc72084335989356096a9ad4e090b23
Izu-TABI <masakisama14@gmail.com>
2022-09-23 21:41:31 +0900

    [Add]jsx for works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -14,21 +14,35 @@ function Works() {
                 <div className="work-block">
                     <a href="https://github.com/Izu-TABI/discord_bot_Log">Log</a>
                     <p>Node.js<br/>
-                    Discordのbotでボイスチャンネルの入退出を記録するのが主な機能。<br/>
-                    他にもボイスチャンネルに誰も参加してない場合メンションする機能などがある。
+                        Discordのbotでボイスチャンネルの入退出を記録するのが主な機能。<br/>
+                        他にもボイスチャンネルに誰も参加してない場合メンションする機能などがある。
                     </p>
                 </div>
                 <div className="work-block">
                     <a href="https://github.com/Izu-TABI/discord_bot_weather">weather</a>
                     <p>Node.js<br/>
-                        Discordのbotで天気予報をAPIを用いて送るbot
+                        Discordのbotで天気予報をAPIで取得して送信するbot
                     </p>
                 </div>
                 <div className="work-block">
                     <a href="https://github.com/Izu-TABI/classic_games">CLASSIC GAMES</a>
                     <p>JavaScript HTML CSS jQuery 98.css<br/>
                         CSSのライブラリである98.cssを用いてwindows98の雰囲気を再現。有名ゲームを3つ収録。
-                        中国地区高専コンピュータフェスティバル(2022)の展示作品。
+                        中国地区高専コンピュータフェスティバル(2022)の時の作品。
+                    </p>
+                </div>
+                <div className="work-block">
+                    <a href="https://github.com/Izu-TABI/bookwalker_scraping">web scraping</a>
+                    <p>Python<br/>
+                        <a href="https://bookwalker.jp/top/?adpcnt=ROxepnj9&utm_source=google&utm_medium=cpc&utm_term=b_bookwalker&utm_campaign=&utm_content=__cp_15587790200_gr_131970067195_kw_aud-1665289761998:kwd-4401790829_cr_569972028628&gclid=Cj0KCQjwsrWZBhC4ARIsAGGUJupgq35i8v1TAFZMw6pr7d1tlx2sl2IxLKWTGwjcb1Fyiat6nn13dOkaAjq4EALw_wcB">BOOKWALKER</a>
+                        の期間限定価格の商品をスクレイピングで取得し、csvファイルに書き出す。
+                    </p>
+                </div>
+                <div className="work-block">
+                    <a href="https://github.com/Izu-TABI/speak_clock">speak clock</a>
+                    <p>JavaScript<br/>
+                        時刻を取得して読み上げるというもの。ネタ。<br/>
+                        <a href="https://izu-tabi.github.io/speak_clock/">公開リンク</a>
                     </p>
                 </div>
             </div>
