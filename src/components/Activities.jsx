0f60fcf9752be6d009046ad396fc437cbd5985ef
Izu-TABI <masakisama14@gmail.com>
2022-09-23 13:40:19 +0900

    [Add]element for activities

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -27,11 +27,23 @@ function Activities() {
                     <div className="block-txt">
                         <div className="block-date">
                             <p>
-                                <span className="date">2022.3</span>
+                                <span className="date">2022.9</span>
                             </p>
                         </div>
                         <p className="tit">
-                            中国地区高専コンピュータフェスティバル - ゲーム部門
+                            パソコン甲子園2022 - プログラミング部門
+                        </p>
+                        <div className="block-txt"></div>
+                    </div>
+
+                    <div className="block-txt">
+                        <div className="block-date">
+                            <p>
+                                <span className="date">2022.9</span>
+                            </p>
+                        </div>
+                        <p className="tit">
+                            第22回日本情報オリンピック
                         </p>
                         <div className="block-txt"></div>
                     </div>
