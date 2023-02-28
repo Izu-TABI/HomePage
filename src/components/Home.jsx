b2d58b6a30fa6d253775e62bfae537029631c4fc
Izu-TABI <masakisama14@gmail.com>
2023-02-28 21:48:31 +0900

    [Update] style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import '../HomeAnimation.css';
-import GithubIcon from '../sns_icons_image/GitHub.png';
-import instaIcon from '../sns_icons_image/instagram.png';
-import twitterIcon from '../sns_icons_image/Twitter.png';
-import blogIcon from '../sns_icons_image/blog_icon.png';
 import Hamburger from './Hamburger';
   
 function Home() {
@@ -11,9 +7,7 @@ function Home() {
     <div className='home-main' style={{position: 'relative'}}>
         <Hamburger />
         <h1 className='home-main-title'>Izu-TABI</h1>
-        <div style={{display: 'flex', justifyContent: 'center'}}>
-        <small style={{color: 'gray', fontSize: '10px', position: 'absolute', bottom: '0', marginBottom: '6px'}}>Copyright © 2023 Izu-TABI All rights reserved.</small>
-        </div>
+          <small style={{color: 'gray', fontSize: '10px', position: 'absolute', bottom: '0', marginBottom: '6px'}}>Copyright © 2023 Izu-TABI All rights reserved.</small>
     </div>
   )
 }
