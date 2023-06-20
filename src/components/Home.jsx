984bdc8dd3b912d52eb04bca2329341406e19bc9
Izu-TABI <masakisama14@gmail.com>
2023-06-20 21:46:41 +0900

    [Update] CSS for copyright

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../HomeAnimation.css';
 import Hamburger from './Hamburger';
-  
+
 function Home() {
   return (
-    <div className='home-main' style={{position: 'relative'}}>
-        <Hamburger />
-        <h1 className='home-main-title'>Izu-TABI</h1>
-          <small style={{color: 'gray', fontSize: '10px', position: 'absolute', bottom: '0', marginBottom: '6px'}}>Copyright © 2023 Izu-TABI All rights reserved.</small>
+    <div className='home-main' style={{ position: 'relative' }}>
+      <Hamburger />
+      <h1 className='home-main-title'>Izu-TABI</h1>
+      <small style={{ color: 'gray', fontSize: '10px', position: 'absolute', bottom: '0', marginBottom: '20px' }}>Copyright © 2023 Izu-TABI All rights reserved.</small>
     </div>
   )
 }
