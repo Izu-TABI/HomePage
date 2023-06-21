b877889aca6af4980b4c5caf3e6e00739b128e86
Izu-TABI <masakisama14@gmail.com>
2023-06-21 11:41:11 +0900

    [Update] style

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -2,92 +2,92 @@ import React from 'react'
 import Hamburger from './Hamburger'
 
 function Activities() {
-  return (
-    <>
-        <div className="main">
-            <Hamburger />
-            <h2 className="title">
+    return (
+        <>
+            <div className="main">
+                <Hamburger />
+                <h2 className="title">
                     Activities
-            </h2>
+                </h2>
 
-            <div className="contents">
-                <div className="block">
-                    <div className="block-txt">
-                        <div className="block-date">
-                            <p>
-                                <span className="date">2022.3</span>
-                            </p> </div>
-                        <p className="tit">
-                            中国地区高専コンピュータフェスティバル - ゲーム部門
-                        </p>
-                        <div className="block-txt"></div>
-                    </div>
-
-                    <div className="block-txt">
-                        <div className="block-date">
-                            <p>
-                                <span className="date">2022.9</span>
+                <div className="contents">
+                    <div className="block">
+                        <div className="block-txt">
+                            <div className="block-date">
+                                <p>
+                                    <span className="date">2022.3</span>
+                                </p> </div>
+                            <p className="tit">
+                                中国地区高専コンピュータフェスティバル - ゲーム部門
                             </p>
+                            <div className="block-txt"></div>
                         </div>
-                        <p className="tit">
-                            パソコン甲子園2022 - プログラミング部門
-                        </p>
-                        <div className="block-txt"></div>
-                    </div>
 
-                    <div className="block-txt">
-                        <div className="block-date">
-                            <p>
-                                <span className="date">2022.9</span>
+                        <div className="block-txt">
+                            <div className="block-date">
+                                <p>
+                                    <span className="date">2022.9</span>
+                                </p>
+                            </div>
+                            <p className="tit">
+                                パソコン甲子園2022 - プログラミング部門
                             </p>
+                            <div className="block-txt"></div>
                         </div>
-                        <p className="tit">
-                            第22回日本情報オリンピック
-                        </p>
-                        <div className="block-txt"></div>
-                    </div>
 
-                    <div className="block-txt">
-                        <div className="block-date">
-                            <p>
-                                <span className="date">2022.10</span>
+                        <div className="block-txt">
+                            <div className="block-date">
+                                <p>
+                                    <span className="date">2022.9</span>
+                                </p>
+                            </div>
+                            <p className="tit">
+                                第22回日本情報オリンピック
                             </p>
+                            <div className="block-txt"></div>
                         </div>
-                        <p className="tit">
-                            技育祭2022【秋】
-                        </p>
-                        <div className="block-txt"></div>
-                    </div>
 
-                    <div className="block-txt">
-                        <div className="block-date">
-                            <p>
-                                <span className="date">2023.3</span>
+                        <div className="block-txt">
+                            <div className="block-date">
+                                <p>
+                                    <span className="date">2022.10</span>
+                                </p>
+                            </div>
+                            <p className="tit">
+                                技育祭2022【秋】
                             </p>
+                            <div className="block-txt"></div>
                         </div>
-                        <p className="tit">
-                  				中国地区高専コンピュータフェスティバル - アプリケーション部門 
-                        </p>
-                        <div className="block-txt"></div>
-                    </div>
 
-                  <div className="block-txt final-contents">
-                        <div className="block-date">
-                            <p>
-                                <span className="date">2023.3</span>
+                        <div className="block-txt">
+                            <div className="block-date">
+                                <p>
+                                    <span className="date">2023.3</span>
+                                </p>
+                            </div>
+                            <p className="tit">
+                                中国地区高専コンピュータフェスティバル - アプリケーション部門
                             </p>
+                            <div className="block-txt"></div>
                         </div>
-                        <p className="tit">
-                        </p>
-                          技育祭2023【春】
-                        <div className="block-txt"></div>
-                    </div>
-                  </div>
 
+                        <div className="block-txt final-contents">
+                            <div className="block-date">
+                                <p>
+                                    <span className="date">2023.3</span>
+                                </p>
+                            </div>
+                            <p className="tit">
+                                技育祭2023【春】
+                            </p>
+                            <div className="block-txt"></div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Activities
