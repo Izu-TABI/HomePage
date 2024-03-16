8699a5ca68f096bac666399678cb449a6d672a13
Izu-TABI <masakisama14@gmail.com>
2024-03-16 23:12:01 +0900

    feat: add loading

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import Works from './components/Works';
 import Activities from './components/Activities';
 import Contact from './components/Contact';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Loading from './components/Loading';
+import { Suspense } from 'react';
 
 function App() {
 
   return (
     <div>
       <Router>
-        <div className="contaier">
-          <div className="main">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/likes" element={<Likes />} />
-              <Route path="/works" element={<Works />} />
-              <Route path="/activities" element={<Activities />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
+        <Suspense fallback={<Loading />}>
+          <div className="contaier">
+            <div className="main">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/likes" element={<Likes />} />
+                <Route path="/works" element={<Works />} />
+                <Route path="/activities" element={<Activities />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </div>
           </div>
-        </div>
+        </Suspense>
+
       </Router>
     </div>
   );
